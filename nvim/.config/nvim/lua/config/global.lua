6f745ef6bbfa45c85bc6a8206aa9937012f930ee
Gangolf <friederrodewald@web.de>
2024-01-19 11:22:47 +0100

    add descriptions

--- nvim/.config/nvim/lua/config/global.lua
+++ nvim/.config/nvim/lua/config/global.lua
@@ -1,72 +1,70 @@
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true              -- Enable true color support if the terminal supports it.
 
--- more opinionated
-vim.opt.number = true                           -- show linenumbers
-vim.opt.relativenumber = true                   -- how relative line numbers
-vim.opt.cursorline = true                       -- highlight the current line
-vim.opt.ruler = false                           -- hide the line and column number of the cursor position
-vim.opt.numberwidth = 3                         -- minimal number of columns to use for the line number {default 4}
-vim.opt.mouse = 'a'                             -- enable mouse
-vim.opt.clipboard:append 'unnamedplus'          -- use system clipboard
-vim.opt.backup = false                          -- creates a backup file
-vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
-vim.o.timeout = true                            -- see next
-vim.opt.timeoutlen = 600                        -- time to wait for a mapped sequence to complete (in milliseconds); also for which-key
-vim.opt.updatetime = 150                        -- for autocommands and hovers
-vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- minimal number of screen lines to keep above and below the cursor
-vim.opt.sidescrolloff = 24                      -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
+-- More opinionated settings:
+vim.opt.number = true                     -- Show line numbers.
+vim.opt.relativenumber = true             -- Show relative line numbers.
+vim.opt.cursorline = true                 -- Highlight the current line.
+vim.opt.ruler = false                     -- Hide the line and column number of the cursor position.
+vim.opt.numberwidth = 3                   -- Set the minimal number of columns to use for the line number.
+vim.opt.mouse = 'a'                       -- Enable mouse support.
+vim.opt.clipboard:append 'unnamedplus'    -- Use the system clipboard.
+vim.opt.backup = false                    -- Disable creating backup files.
+vim.opt.fileencoding = "utf-8"            -- Set the default file encoding to UTF-8.
+vim.o.timeout = true                      -- Enable 'timeout' behavior (see below).
+vim.opt.timeoutlen = 600                  -- Set the time to wait for a mapped sequence to complete (in milliseconds).
+vim.opt.updatetime = 100                  -- Set the time to wait for autocommands and hovers.
+vim.opt.undofile = true                   -- Enable persistent undo.
+vim.opt.wrap = false                      -- Display lines as one long line.
+vim.opt.scrolloff = 8                     -- Set the minimal number of screen lines to keep above and below the cursor.
+vim.opt.sidescrolloff = 24                -- Set the minimal number of screen columns to keep to the left and right of the cursor if wrap is false.
 
--- don't ask about existing swap files
-vim.opt.shortmess:append 'A'
+vim.opt.shortmess:append 'A'              -- Don't ask about existing swap files.
 
--- use spaces as tabs
+-- Use spaces instead of tabs:
 local tabsize = 6
-vim.opt.expandtab = true
-vim.opt.shiftwidth = tabsize
-vim.opt.tabstop = tabsize
+vim.opt.expandtab = true                  -- Use spaces instead of tabs for indentation.
+vim.opt.shiftwidth = tabsize              -- Number of spaces for each level of indentation.
+vim.opt.tabstop = tabsize                 -- Number of spaces that a <Tab> counts for.
 
--- space as leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Space as leader and local leader:
+vim.g.mapleader = " "                     -- Set global leader key to space.
+vim.g.maplocalleader = " "                -- Set local leader key to space.
 
--- smarter search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+-- Smarter search settings:
+vim.opt.ignorecase = true                 -- Make searches case-insensitive by default.
+vim.opt.smartcase = true                  -- Override 'ignorecase' when the search pattern contains uppercase characters.
 
--- indent
-vim.opt.smartindent = true
-vim.opt.breakindent = true
+-- Indent settings:
+vim.opt.smartindent = true                -- Enable smart auto-indenting.
+vim.opt.breakindent = true                -- Enable auto-indenting on wrapped lines.
 
--- consistent number column
-vim.opt.signcolumn = "yes:1"
+-- Consistent number column:
+vim.opt.signcolumn = "yes:1"              -- Always show the number column, even when there are no line numbers.
 
--- how to show autocomplete menu
-vim.opt.completeopt = 'menuone,noinsert'
+-- How to show autocomplete menu:
+vim.opt.completeopt = 'menuone,noinsert'  -- Show a menu with possible completions, but do not automatically insert.
 
--- add folds with treesitter grammar
-vim.opt.foldmethod = "expr"
-vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- but open all by default
-vim.opt.foldlevel = 99
+-- Add folds with treesitter grammar:
+vim.opt.foldmethod = "expr"               -- Set foldmethod to expression.
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'  -- Use treesitter grammar for folding.
+vim.opt.foldlevel = 99                    -- Open all folds by default.
 
--- global statusline
-vim.opt.laststatus = 3
+-- Global statusline:
+vim.opt.laststatus = 3                    -- Always show the statusline.
 
--- split right and below by default
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+-- Split right and below by default:
+vim.opt.splitright = true                 -- Open new split windows to the right by default.
+vim.opt.splitbelow = true                 -- Open new split windows below by default.
 
---tabline
-vim.opt.showtabline = 0
+-- Tabline settings:
+vim.opt.showtabline = 0                   -- Hide the tabline.
 
---windowline
--- vim.opt.winbar = '%f'
+-- Windowline settings:
+-- vim.opt.winbar = '%f'                  -- Show file names in the window statusline.
 
---don't continue comments automagically
-vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
+-- Don't continue comments automagically:
+vim.opt.formatoptions:remove({ 'c', 'r', 'o' })  -- Disable auto-comment continuation, auto-wrap, and comment formatting.
 
--- hide cmdline when not used
-vim.opt.cmdheight = 1
+-- Hide cmdline when not used:
+vim.opt.cmdheight = 1            -- Set the command-line height to 1, hiding it when not in use.
 
