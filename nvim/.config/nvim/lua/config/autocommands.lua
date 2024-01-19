0e57a5897ce14c6030bcc56a0eacb19795f7ddc7
Gangolf <friederrodewald@web.de>
2024-01-19 10:44:00 +0100

    update autocommands and give description

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -1,27 +1,11 @@
-local function set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-end
-
+-- ensure that the buffer reflects the latest changes
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
   pattern = { "*" },
   command = "checktime",
 }
 )
 
-vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  pattern = { "*" },
-  callback = function(_)
-    vim.cmd.setlocal 'nonumber'
-    set_terminal_keymaps()
-  end
-}
-)
-
+-- sets up a key mapping for closing the buffer and ensures that the buffer is not listed in the buffer list
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
   callback = function()
@@ -32,41 +16,13 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = { "gitcommit", "markdown" },
---   callback = function()
---     vim.opt_local.wrap = true
---     vim.opt_local.spell = true
---   end,
--- })
-
--- Automatically close tab/vim when nvim-tree is the last window in the tab
+-- automatically close tab/vim when nvim-tree is the last window in the tab
 vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
 
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-  callback = function()
-    vim.cmd "tabdo wincmd ="
-  end,
-})
-
+-- highlight visually selected text for a brief duration after a yank operation 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
-
--- vim.api.nvim_create_autocmd({ "VimEnter" }, {
---   callback = function()
---     vim.cmd "hi link illuminatedWord LspReferenceText"
---   end,
--- })
-
--- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
---   callback = function()
---     local line_count = vim.api.nvim_buf_line_count(0)
---     if line_count >= 5000 then
---       vim.cmd "IlluminatePauseBuf"
---     end
---   end,
--- })
 
