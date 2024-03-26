beb154b33eafaa455b98b0da8e0a11e5c300e234
gangolf <friederrodewald@web.de>
2024-03-26 16:17:41 +0100

    make work tmux arrows

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -22,14 +22,6 @@ end
 
 -- normal mode
 for _, key in pairs({
-    -- "<C-h>",
-    -- "<C-j>",
-    -- "<C-k>",
-    -- "<C-l>",
-    -- "<C-Up>", -- resize window using <ctrl> arrow keys
-    -- "<C-Down>",
-    -- "<C-Left>",
-    -- "<C-Right>",
     "<S-h>", -- prevBuffer
     "<S-l>", -- nextBuffer
     "[b", -- prevBuffer
@@ -95,12 +87,6 @@ map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-
--- -- move TO window using the <ctrl> arrow keys
--- map("n", "<C-Left>", "<C-w>h", { desc = "Go to left window" })
--- map("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window" })
--- map("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window" })
--- map("n", "<C-Right>", "<C-w>l", { desc = "Go to right window" })
 
 -- close file
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "[Q]uit" })
