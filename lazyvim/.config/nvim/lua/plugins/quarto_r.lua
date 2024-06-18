b7bdb45b2c3c6cfd90927f1c24173b25f72c45c7
gangolf <friederrodewald@web.de>
2024-06-18 17:20:25 +0200

    add radian instead of R

--- lazyvim/.config/nvim/lua/plugins/quarto_r.lua
+++ lazyvim/.config/nvim/lua/plugins/quarto_r.lua
@@ -3,6 +3,7 @@ return {
         "R-nvim/R.nvim",
         opts = {
             R_app = "radian",
+            R_args = { "--ask-save" },
             external_term = 'tmux split-window -h -c "#{pane_current_path}"',
             config_tmux = true,
         },
